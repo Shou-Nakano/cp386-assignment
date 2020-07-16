9de3ed4a4da03faf6ef56a4ddac218e7fdcbe3dc
Shou-Nakano <shournakano@gmail.com>
2020-07-16 11:56:35 -0400

    Update 180372910_yourid_a04.c
    Resource Count works.

--- 180372910_yourid_a04.c
+++ 180372910_yourid_a04.c
@@ -3,8 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+int readFile (char* fileName);
+void RQ();
+void RL();
+void asterisk();
+void run();
+int safetyAlgorithm();
 
-int main() {
+// The vectors/matrices will be defined here as global variables.
+
+int main(int argc, char *argv[]) {
 	
 	if(argc<2)
 	{
@@ -12,22 +20,42 @@ int main() {
 		return -1;
 	}
 
-
 	readFile(argv[1]);
-	
+	// Prompt the user for input here. We should assume that using the run command ends the program.
+
+	return 0;
 }
 
 int readFile(char* fileName) // Reads the input file and sets up the vectors/matrices.
 {
-	// How many lines are in the file?
 	FILE *in = fopen(fileName, "r");
+	char filePointer;
+	int lines;
+	int commas;
+	int resources; // Resources should equal (commas+lines)/lines.
 
 	if(!in)
 	{
 		printf("Child A: Error in opening input file...exiting with error code -1\n");
 		return -1;
 	}
+	// How many lines/commas and by extension, resources are in the file?
 
+	while (filePointer != EOF)
+    {
+        if (filePointer == '\n'){
+            lines = lines + 1; // Increment the line count.
+        }
+		if (filePointer == ','){
+			commas = commas + 1; // Increment the comma count.
+		}
+        //Move to the next character.
+        filePointer = getc(in);
+    }
+
+	resources = (commas+lines)/lines;
+	printf("%d", resources); // A test print.
+	return 0;
 }
 
 void RQ(){
@@ -49,5 +77,5 @@ void run(){ // This function should use safetyAlgorithm to check to see if there
 }
 
 int safetyAlgorithm(){ // This function should contain the safety algorithm that will be called by RQ, RL and run and will update the vector containing the safe sequence. If there is a safe sequence, return 1, if not, return 0. This should also update a vector containing the safe sequence.
- 	
+ 	return 1;
 }

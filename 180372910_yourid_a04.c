0c43ccef42e15e2cb69bb3018917f1fa5de146bd
Shou-Nakano <shournakano@gmail.com>
2020-07-16 11:31:20 -0400

    Update 180372910_yourid_a04.c

--- 180372910_yourid_a04.c
+++ 180372910_yourid_a04.c
@@ -4,8 +4,19 @@
 #include <string.h>
 
 
-
 int main() {
-
 	
+	if(argc<2)
+	{
+		printf("Input file name missing...exiting with error code -1\n");
+		return -1;
+	}
+
+
+	readFile();
+	
+}
+
+void readFile(){
+
 }

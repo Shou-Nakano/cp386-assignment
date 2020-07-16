33de0023fd645039f31b5ebbc21d8d2c229d8df0
Shou-Nakano <shournakano@gmail.com>
2020-07-16 14:53:36 -0400

    Update 180372910_yourid_a04.c

--- 180372910_yourid_a04.c
+++ 180372910_yourid_a04.c
@@ -10,7 +10,7 @@ void asterisk();
 void run();
 int safetyAlgorithm();
 
-public int done = false;
+int done = 0;
 int *available = NULL; // 1D vector for resources, which stores the number of available resources of each type. If available [j] = k, there are k instances of resource type Rj available.
 int ** max = NULL; // 2D array (number of processes by number of resources). If Max [i,j] = k, then process Timay request at most k instances of resource type Rj.
 int ** allocation = NULL; // 2D array (number of processes by number of resources). If Allocation[i,j] = k then Ti is currently allocated k instances of Rj.
@@ -29,9 +29,9 @@ int main(int argc, char *argv[]) {
 
 	readFile("sample4_in.txt");
 	// Prompt the user for input here. We should assume that using the run command ends the program.
-	while (done == false){
+	/*while (done == 0){
 		
-	}
+	}*/
 
 	free(available); // Don't forget to free everything at the end of the program.
 	return 0;
@@ -39,11 +39,13 @@ int main(int argc, char *argv[]) {
 
 int readFile(char* fileName) // Reads the input file and sets up the vectors/matrices.
 {
+	printf("readFile started.");
 	FILE *in = fopen(fileName, "r");
 	char filePointer;
 	int lines;
 	int commas;
 	int resources; // Resources should equal (commas+lines)/lines.
+	int processes;
 
 	if(!in)
 	{
@@ -63,11 +65,14 @@ int readFile(char* fileName) // Reads the input file and sets up the vectors/mat
         //Move to the next character.
         filePointer = getc(in);
     }
-
+	processes = lines;
 	resources = (commas+lines)/lines; // This works, for sample4_in.txt, it printed 4.
 	// Next, dynamically set up the vectors/matrices.
 	available = (int *)malloc(resources * sizeof(int)); // Create the Available vector.
-
+	max = (int **)malloc(processes * sizeof(int *));
+	for (int i=0; i<processes; i++){
+         max[i] = (int *)malloc(resources * sizeof(int)); // Create the max matrix.
+	}
 	// Finally, fill the contents of those vectors/matrices.
 
 	return 0;
@@ -88,7 +93,7 @@ void asterisk(){
 }
 
 void run(){ // This function should use safetyAlgorithm to check to see if there is a safe series of threads and if so, 'run' them as seen in the sample output.
-	done = true;
+	done = 1;
 }
 
 int safetyAlgorithm(){ // This function should contain the safety algorithm that will be called by RQ, RL and run and will update the vector containing the safe sequence. If there is a safe sequence, return 0, if not, return -1. This should also update a vector containing the safe sequence.
